b84448bd50bbbeea28cded3d9619b574fd3b05ce
Santosh177 <117721403+Santosh177@users.noreply.github.com>
2023-02-28 22:24:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//your code here
 //your code here
 let nextButton = document.querySelector("#load_next");
 let previousButton = document.querySelector("#load_prev");
@@ -41,49 +41,3 @@ async function getGithubIssuesTitle(pageNum = 1) {
   const result = await response.json();
   return result.map((obj) => obj.title);
 }
-
-// on page load
-getGithubIssuesTitle().then((data) => renderLists(data));
-
-
-
-// const pNoSpan = document.getElementById("pNo"),
-//   listElement = document.getElementById("list"),
-//   nextBtn = document.getElementById("load_next"),
-//   prevBtn = document.getElementById("load_prev");
-
-// let pageNumber = 1;
-
-// const renderIssues = (issues) => {
-//   while (listElement.firstChild) {
-//     listElement.removeChild(listElement.firstChild);
-//   }
-//   issues.forEach((issue) => {
-//     const li = document.createElement("li");
-//     li.textContent = issue.title;
-//     listElement.appendChild(li);
-//   });
-// };
-
-// const fetchIssues = async () => {
-//   pNoSpan.textContent = pageNumber;
-//   const url = `https://api.github.com/repositories/1296269/issues?page=${pageNumber}&per_page=5`;
-//   const res = await fetch(url);
-//   const data = await res.json();
-
-//   renderIssues(data);
-// };
-
-// const handleNextClick = () => {
-//   pageNumber += 1;
-//   fetchIssues();
-// };
-
-// const handlePrevClick = () => {
-//   pageNumber -= 1;
-//   fetchIssues();
-// };
-
-// document.addEventListener("DOMContentLoaded", fetchIssues);
-// nextBtn.addEventListener("click", handleNextClick);
-// prevBtn.addEventListener("click", handlePrevClick);
